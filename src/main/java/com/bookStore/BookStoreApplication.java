dc78b2468709ce5f82e01e8c0b4640cd1c36b1a7
HP <HP@DESKTOP-EN54CJO>
2024-03-18 20:12:22 +0530

    MyFirstCommit

---
+++ src/main/java/com/bookStore/BookStoreApplication.java
@@ -0,0 +1,15 @@
+package com.bookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApplication.class, args);
+		System.out.println("The application is running");
+		System.out.println("the testing")	;
+		}
+
+}
